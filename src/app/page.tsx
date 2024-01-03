906be272aabe5fcd378b6a2bd7aeab732abbee4e
Tyronnosaurus <Tyronnosaurus@protonmail.com>
2024-01-04 00:25:47 +0100

    Added responsiveness on small screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,9 +11,9 @@ export default function Home() {
   return (
     <main className="">
 
- 
-
-      <div className="flex flex-row justify-center items-center">
+      <div className="flex flex-col align-middle
+                      md:flex-row justify-center items-center">
+        
         <Image
           src="/imatge_portada.webp"
           alt="Persona escalant"
@@ -21,10 +21,12 @@ export default function Home() {
           height="568"
         />
 
-        <div className='ml-20'>
+        <div className='mt-10 mb-20 md:ml-20'>
           <FileBrowser json={json} />
         </div>
+
       </div>
+
     </main>
   )
 }
