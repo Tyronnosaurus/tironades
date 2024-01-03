0ddb0516f685bc869a6d000de3da12f026097b0d
Tyronnosaurus <Tyronnosaurus@protonmail.com>
2024-01-04 00:04:32 +0100

    Prepared for build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <h1 className="text-center text-4xl md:text-5xl font-bold m-8">Tironades</h1>
         <div className="m-auto w-[50%]">
-          <h2 className="text-center text-1xl md:text-2xl mt-8">40 anys d'activitats pioneres al massís del Port i serres veïnes</h2>
+          <h2 className="text-center text-1xl md:text-2xl mt-8">40 anys d&apos;activitats pioneres al massís del Port i serres veïnes</h2>
           <h2 className="text-right text-1xl md:text-2xl mb-8">- Joan Tiron i companys -</h2>
         </div>
         {children}
