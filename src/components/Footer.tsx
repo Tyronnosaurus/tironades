9deca2b853a402ec465d4ecde8dae7a680485532
Tyronnosaurus <Tyronnosaurus@protonmail.com>
2024-01-05 01:40:34 +0100

    Added "Designed by" to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <footer className="text-center w-full h-14 mt-10
                        bg-white border-t border-slate-300">
-      &copy; {(new Date().getFullYear())} Joan Josep Tiron
+      &copy; {(new Date().getFullYear())} Joan Josep Tiron; Dissenyat per Eduard Tiron
     </footer>
   )
 
