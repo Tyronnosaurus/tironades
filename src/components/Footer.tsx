e32b9cce9fd3310678778a6dcf660160897c0e3d
Tyronnosaurus <Tyronnosaurus@protonmail.com>
2024-01-05 01:43:14 +0100

    Cntered footer vertically

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,8 +5,9 @@ const Footer = () => {
 
   return (
     <footer className="text-center w-full h-14 mt-10
+                       flex flex-col justify-center
                        bg-white border-t border-slate-300">
-      &copy; {(new Date().getFullYear())} Joan Josep Tiron; Dissenyat per Eduard Tiron
+      &copy;{(new Date().getFullYear())} Joan Josep Tiron; Dissenyat per Eduard Tiron
     </footer>
   )
 
